a436b66e6cb8efc58bc4e09c204c87ce2a939f71
Ryan Bailey <rybailey@meta.com>
2023-01-12 10:56:54 -0800

    Toolkit copyright header fixes
    Summary: Fixed Toolkit copyright issues
    
    Reviewed By: Barbog
    
    Differential Revision: D42480029
    
    fbshipit-source-id: 0bc7bd65f462168f508d3d5b5e0af6837cf0cd20

--- Samples/Gallery/Scripts/Runtime/Contents/WindWheel.cs
+++ Samples/Gallery/Scripts/Runtime/Contents/WindWheel.cs
@@ -1,5 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 using UnityEngine;
 
 namespace Oculus.Voice.Toolkit.Samples
